cea566b64f10e6fd3eed581571edc2f72e6606bb
Paul Lindén <linden.91.pl@gmail.com>
2025-02-17 21:12:40 +0100

    fixed verify

--- src/components/VerifyEmail.jsx
+++ src/components/VerifyEmail.jsx
@@ -1,24 +1,29 @@
 import { useEffect, useState } from "react";
-import { getAuth, applyActionCode } from "firebase/auth";
+import { getAuth, applyActionCode, signOut } from "firebase/auth";
 
-const VerifyEmail = ({oobCode}) => {
+const VerifyEmail = ({ oobCode }) => {
     const auth = getAuth();
     const [message, setMessage] = useState("");
-
-    useEffect(() => {
-        if (oobCode) {
-            applyActionCode(auth, oobCode)
-                .then(() => setMessage("E-postadressen har verifierats!"))
-                .catch(() => setMessage("Misslyckades med att verifiera e-postadressen."));
+ 
+    const handleVerifyEmail = async () => {
+        try {
+            await applyActionCode(auth, oobCode);
+            console.log("E-postverifiering lyckades!");
+            setMessage("E-postverifiering lyckades!");
+        } catch (error) {
+            console.error("Fel vid verifiering av e-post:", error);
+            setMessage("Fel vid verifiering av e-post.");
+        } finally {
+            signOut(auth);
         }
-    }, [oobCode]);
-
+    };
+        
     return (
         <div>
             {oobCode ? (
                 <>
-                    <h2>Verifiera e-post</h2>
-                    <p>{message}<a href="/"> Till start.</a></p>
+                    {!message && <button onClick={handleVerifyEmail}>Verifiera e-post</button>}
+                    {message && <p>{message}<a href="/"> Till start.</a></p> }
                 </>
             ) : (
                 <p>Något gick fel. <a href="/">Till start.</a></p>
